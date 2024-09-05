a35fb6b936878d7645b1ab5dc351b861ef7d4118
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-05 10:53:49 +0600

    App.compoent with route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-export default function App() {
+// src/App.js
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import AddJob from './components/AddJob/AddJob';
+import Home from './components/Home/Home';
+import Layout from './components/Layout/Layout';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="add-job" element={<AddJob />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
