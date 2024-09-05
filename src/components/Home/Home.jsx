0a843a3ffb4c1ae5a18b90a6c0386cd976c9b454
Shariar Duke <shariarmahmud225@gmail.com>
2024-09-05 11:37:12 +0600

    upper part of Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,26 @@
+// src/components/Home/Home.js
 export default function Home() {
     return (
-      <div className="text-white p-4">
-        <div className="flex items-center justify-between">
+      <div className="text-white p-4 max-w-4xl mx-auto">
+        <div className="flex items-center justify-between gap-4">
           <h1 className="font-bold text-[32px]">All Available Jobs</h1>
-          <div className="">
+          <div className="flex gap-4 items-center">
             <input 
               type="search" 
-              className="p-2 rounded-lg bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500" 
+              className="w-48 p-2 rounded-lg bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500" 
               placeholder="Search..."
             />
+            <select 
+              className="w-48 p-2 rounded-lg bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500" 
+              name="salary" 
+              id="salary"
+            >
+              <option value="default">Default</option>
+              <option value="salary-low-to-high">Salary (Low to High)</option>
+              <option value="salary-high-to-low">Salary (High to Low)</option>
+            </select>
           </div>
         </div>
       </div>
     );
-  }
-  
+}
